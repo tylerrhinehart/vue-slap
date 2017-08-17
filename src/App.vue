cedb00800b268005d03e3fbe9c53393d5a14a673
Tyler Rhinehart <rhinehart22@gmail.com>
2017-08-16 19:50:44 -0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="app">
+    <button @click="resetAll">Reset All</button>
+    <div v-for="opponent in opponents">
+      <slap :target="opponent"></slap>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Slap from './components/Slap'
+
+  export default {
+    name: 'app',
+    data() {
+      return {
+        opponents: [
+          { name: 'Mark', health: 100, img: '//media.licdn.com/mpr/mpr/shrinknp_200_200/AAEAAQAAAAAAAAKPAAAAJDIyNmZlYWIxLTUwZjYtNDg2My04N2Q3LTJiODBhMzIwYWVhOA.jpg' },
+          { name: 'Treebeard', health: 100, img: '//upload.wikimedia.org/wikipedia/en/2/2e/Treebeard.jpg' }
+        ]
+
+      }
+    },
+    methods: {
+      resetAll() {
+        for (var i = 0; i < this.opponents.length; i++) {
+          var opponent = this.opponents[i]
+          opponent.health = 100
+        }
+      }
+    },
+    components: {
+      Slap
+    }
+  }
+
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
---
+++ src/components/Slap.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="hello">
+    <h1 :class="{red: target.health <= 25, green: target.health > 75, orange: target.health <=75 && target.health > 25}">{{ target.name }} {{ target.health }}</h1>
+    <button class="btn btn-primary" v-if="target.health > 0" @click="slap">Slap {{ target.name }}</button>
+    <button class="btn btn-danger" :disabled="target.health <= 0" @click="curbStomp">Curb-stomp {{ target.name }}</button>
+    <button class="btn btn-success" @click="reset">Reset</button>
+    <div>
+        <img :src="target.img">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  props: ['target'],
+  data() {
+    return {
+    }
+  },
+  methods: {
+    slap() {
+      this.target.health -= 1
+      this.checkHealth()
+    },
+    curbStomp() {
+      this.target.health -= 5
+      this.checkHealth()
+    },
+    checkHealth() {
+      if (this.target.health < 0) {
+        this.target.health = 0
+      }
+    },
+    reset() {
+      this.target.health = 100
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.red {
+  color: red;
+}
+
+.green {
+  color: green;
+}
+
+.orange {
+  color: orange;
+}
+img{
+  height: 200px;
+  width: 200px;
+}
+</style>
